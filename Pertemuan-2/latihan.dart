5314616a1d29a98dc480ebdd09bb98c1d79bdd6e
KikyAja31 <2411500032@mahasiswa.atmaluhur.ac.id>
2026-03-13 17:26:05 +0700

    Add: latihan.dart

---
+++ Pertemuan-2/latihan.dart
@@ -0,0 +1,19 @@
+void cekKelulusan(String nama, int nilai) {
+  if(nilai >= 75) {
+    print("Mahasiswa : $nama");
+    print("Nilai : $nilai");
+    print("Selamat anda lulus !!!!");
+    print("===============================");
+  } else {
+    print("Mahasiswa : $nama");
+    print("Nilai : $nilai");
+    print("Maaf, anda tidak lulus !!!!");
+    print("===============================");
+  }
+
+
+}
+void main() {
+  cekKelulusan("Julfandi", 100);
+  cekKelulusan("Mario", 75);
+}
